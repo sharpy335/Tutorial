4a55a85164d79e38f4bf0d2c3b09d7d31ec5323d
sharpy335 <iansharp26@gmail.com>
2016-10-06 16:52:18 +0100

    Add files via upload

---
+++ IS_Jupyter_Notebook20-Copy1.py
@@ -0,0 +1,40 @@
+
+# coding: utf-8
+
+# THIS IS THE ENTRY PYTHON HACKATHON TUTORIAL
+# 
+# Created // Ian Sharp Oct 2016
+# 
+# 
+# 
+# 
+
+# In[1]:
+
+x = 100
+
+
+# In[2]:
+
+y=200
+
+
+# In[3]:
+
+print(x+y)
+
+
+# In[7]:
+
+name = 'john'
+
+
+# In[9]:
+
+name
+
+
+# In[ ]:
+
+
+
